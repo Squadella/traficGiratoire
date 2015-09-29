8b13c113afec40be3cea006f94ccb7803a52a0e9
gregtaole <le@et.esiea.fr>
2015-09-29 12:32:20 +0200

    Added color and designed the central part

--- src/main.c
+++ src/main.c
@@ -10,9 +10,14 @@
 
 int main()
 {
+
     srand(time(NULL));
     char board[51][101];
     listCar existingCar=NULL;
+    struct timespec sleep_time;
+    sleep_time.tv_sec = 0;
+    sleep_time.tv_nsec = 40000000L; //125 000 000
+
     for (int i = 0; i < 51; ++i)
     {
         for(int j=0; j < 101; ++j)
@@ -26,7 +31,7 @@ int main()
         affichage(board);
         printf("%d\n", i);
         ++i;
-        sleep(1);
+        nanosleep(&sleep_time, NULL);
         moveAllCars(board, existingCar);
         existingCar=spawnCar(board, 10, false, existingCar);
     }
